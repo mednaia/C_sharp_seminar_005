3266798d091ff885b0f823b80ecf6fa632aec25e
Ekaterina Tyurina <tiurina.kate@gmail.com>
2022-05-28 23:42:04 +0300

    task003 10 to 2

---
+++ Task003_10_to_2/Program.cs
@@ -0,0 +1,12 @@
+﻿//Написать программу преобразования десятичного числа в двоичное
+Console.Write("Enter the decimal number: ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+int b = 0;
+string result = " ";
+while(a>0)
+{
+    b=a%2;
+    result = b+result;
+    a=a/2;
+}
+Console.Write(result);
