eaaf3cb6e5fe3cc15e300f6d25f250c85077d98e
Ekaterina Tyurina <tiurina.kate@gmail.com>
2022-05-28 23:41:31 +0300

    task002 triangle

---
+++ Task002_Triangle/Program.cs
@@ -0,0 +1,12 @@
+﻿// Выяснить являются ли три числа сторонами треугольника
+Console.Write("Enter the first number: ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Enter the second number: ");
+int b = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Enter the third number: ");
+int c = int.Parse(Console.ReadLine() ?? "0");
+if(a<(b+c) && b<(a+c) && c<(a+b)) Console.WriteLine($"{a},{b},{c} are sides of the triangle.");
+else
+{
+    Console.WriteLine($"{a},{b},{c} aren't sides of the triangle.");
+}
