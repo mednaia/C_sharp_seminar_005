369b6ca0c825b222d18676073f52cca7e9fd7c84
Ekaterina Tyurina <tiurina.kate@gmail.com>
2022-05-28 21:01:50 +0300

    Task001 Function

---
+++ Task001_Function/Program.cs
@@ -0,0 +1,35 @@
+﻿// В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом
+void PrintArray(int[] array)
+{
+    foreach(var element in array)
+        Console.Write($"{element} ");
+    Console.WriteLine();
+}
+
+int[] CreateArray(int size, int minimum, int maximum)
+{
+    int[] array = new int[size];
+    Random random = new Random();
+    for(int i=0;i<size;i++)
+        array[i] = random.Next(minimum,maximum);
+    return array;
+}
+
+bool Contains(int[] array, int value)
+{
+    foreach(var element in array)
+        if(element==value)
+            return true;
+    return false;
+}
+
+int[] firstArray = CreateArray(5,-10, 10);
+Console.Write("Your array: ");
+PrintArray(firstArray);
+Console.Write("Enter value to find in array: ");
+int value = int.Parse(Console.ReadLine() ?? "0");
+//if(Contains(firstArray,value))
+//    Console.WriteLine("Contains");
+//else
+//    Console.WriteLine("Does not contains");
+Console.WriteLine(Contains(firstArray, value) ? "Contains" : "Does not contain");
