dbda6e268f4a597d519b8faf727e9446dfb6321e
Ekaterina Tyurina <tiurina.kate@gmail.com>
2022-05-29 14:24:15 +0300

    Task004 Fibonacci

---
+++ Task004_Fibonacci/Program.cs
@@ -0,0 +1,37 @@
+﻿// Показать числа Фибоначчи меньше заданного числа N
+// f(1)= 1
+// f(2) = 1
+// f(n) = f(n-1) + f(n-2)
+//
+
+double Fibonacci(int n)
+{
+    if(n == 1 || n == 2) return 1;
+    else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+Console.Write("Enter the number: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+int i = 1;
+while(Fibonacci(i)<number)
+{
+    Console.Write(Fibonacci(i));
+    Console.WriteLine();
+    i++;
+}
+
+// Console.Write("Введите число: ");
+// int N = int.Parse(Console.ReadLine()?? "");
+
+// int firstValue = 0;
+// int secondValue = 1;
+// int result = 0;
+// Console.Write($"{firstValue} {secondValue} ");
+
+// while(result < N)
+// {
+//     result = firstValue + secondValue;
+//     if(result > N) break;
+//     Console.Write(result + " ");
+//     firstValue = secondValue;
+//     secondValue = result;
+// }
